b428190ccfa31713dba7554e4a3ac56f4e4022a6
chanyang <chanyang721@gmail.com>
2022-10-19 09:26:52 +0900

    [ feat ] DI helper

--- src/helpers/helper.di.ts
+++ src/helpers/helper.di.ts
@@ -1,1 +1,9 @@
-import tsyringe from 'tsyringe';
+import { autoInjectable, registry, container } from 'tsyringe';
+
+export const Injectable = autoInjectable
+export const Router = autoInjectable
+export const Controller = autoInjectable
+export const Service = autoInjectable
+export const Repository = autoInjectable
+export const Entity = registry
+export const Container = container
